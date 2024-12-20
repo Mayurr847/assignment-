529e30b9805dcbe7b5a6fe67810eebb2e80f2f9d
Mayurr847 <prajapatimayurr564@gmail.com>
2024-12-20 14:34:41 +0530

    Add files via upload

---
+++ M3-ReactJs/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p class="txt">React Js</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <button class="App-btn">Hello world</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
